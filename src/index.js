7b084acfc9717a86410ffae4261f686400dbdcb3
Quang Oliu <trantran30102002@gmail.com>
2023-03-04 16:42:00 +0700

    Static & SCSS

--- src/index.js
+++ src/index.js
@@ -8,14 +8,20 @@ const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
+// Static file
+app.use(express.static(path.join(__dirname, "public")));
+
 // Logger
 app.use(morgan("combined"));
 
 // Engene
-app.engine('hbs', handlebars.engine({
-  extname: '.hbs'
-}));
-app.set('view engine', 'hbs');
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources\\views"));
 
 app.get("/", (req, res) => {
